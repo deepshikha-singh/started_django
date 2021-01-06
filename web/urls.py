9cbd0b364d68c204b771a020f3361ea0364290ca
deepshikha-singh <deepshikha20k@gmail.com>
2021-01-06 18:42:57 +0530

    urls and views

---
+++ web/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', views.index, name='index'),
+
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
